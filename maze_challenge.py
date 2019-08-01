6d6660fa08292060c48d37ff8fa8c2c3d8838e52
hayder <hayder@modernecotech.com>
2019-08-01 14:09:55 +0100

    Initial commit

---
+++ maze_challenge.py
@@ -0,0 +1,25 @@
+#archangel maze challenge
+
+'''
+A maze consists of a block of cells, defined as a n by m matrix. The cells within the maze are
+either X, O, or S. An S denotes the starting point, an O denotes an open cell which can be
+passed through, and an X denotes a wall, which cannot be passed through. The game is
+complete when the S reaches the edge of the maze. Consider the following example of a 6 *
+5 maze:
+XOXXXX
+XOXOOX
+XOXOXX
+XOOOSX
+XXXXXX
+Write code in a language of your choice which will solve mazes similar to the one in the
+example above. You do not have to find the shortest path to the exit.
+Optional bonus tasks you could attempt or describe how you would approach:
+● Find the shortest path
+● Find all paths
+● Optimise your algorithm to find a path (any path) as quickly as possible
+● Expand to three dimensional mazes
+Deliver as source code with a readme explaining how to execute it.
+It is expected to spend no more than a few hours on this task.
+'''
+
+
